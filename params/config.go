147d6a919f2719af041503971623aea344f145a7
btcsc <smartchainbitcoin@gmail.com>
2024-10-12 03:19:11 +0530

    configs

--- params/config.go
+++ params/config.go
@@ -26,7 +26,9 @@ import (
 
 // Genesis hashes to enforce below configs on.
 var (
-	MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
+	MainnetGenesisHash = common.HexToHash("0xd7d18f7c51ab75059ddfb70fa88408ba53f94378339d185899c5819b1a3d5c98")
+
+	// MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
 	HoleskyGenesisHash = common.HexToHash("0xb5f7f912443c940f21fd611f12828d75b534364ed9e95ca4e307729a4661bde4")
 	SepoliaGenesisHash = common.HexToHash("0x25a5cc106eea7138acab33231d7160d69cb777ee0c2c553fcddf5138993e6dd9")
 	GoerliGenesisHash  = common.HexToHash("0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a")
@@ -36,30 +38,60 @@ func newUint64(val uint64) *uint64 { return &val }
 
 var (
 	MainnetTerminalTotalDifficulty, _ = new(big.Int).SetString("58_750_000_000_000_000_000_000", 0)
+	// MainnetTerminalTotalDifficulty, _ = new(big.Int).SetString("0", 0)
 
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainID:                       big.NewInt(1),
-		HomesteadBlock:                big.NewInt(1_150_000),
-		DAOForkBlock:                  big.NewInt(1_920_000),
+		ChainID:                       big.NewInt(0),
+		HomesteadBlock:                big.NewInt(0),
+		DAOForkBlock:                  big.NewInt(0),
 		DAOForkSupport:                true,
-		EIP150Block:                   big.NewInt(2_463_000),
-		EIP155Block:                   big.NewInt(2_675_000),
-		EIP158Block:                   big.NewInt(2_675_000),
-		ByzantiumBlock:                big.NewInt(4_370_000),
-		ConstantinopleBlock:           big.NewInt(7_280_000),
-		PetersburgBlock:               big.NewInt(7_280_000),
-		IstanbulBlock:                 big.NewInt(9_069_000),
-		MuirGlacierBlock:              big.NewInt(9_200_000),
-		BerlinBlock:                   big.NewInt(12_244_000),
-		LondonBlock:                   big.NewInt(12_965_000),
-		ArrowGlacierBlock:             big.NewInt(13_773_000),
-		GrayGlacierBlock:              big.NewInt(15_050_000),
-		TerminalTotalDifficulty:       MainnetTerminalTotalDifficulty, // 58_750_000_000_000_000_000_000
-		TerminalTotalDifficultyPassed: true,
-		ShanghaiTime:                  newUint64(1681338455),
-		Ethash:                        new(EthashConfig),
+		EIP150Block:                   big.NewInt(0),
+		EIP155Block:                   big.NewInt(0),
+		EIP158Block:                   big.NewInt(0),
+		ByzantiumBlock:                big.NewInt(0),
+		ConstantinopleBlock:           big.NewInt(0),
+		PetersburgBlock:               big.NewInt(0),
+		IstanbulBlock:                 big.NewInt(0),
+		MuirGlacierBlock:              big.NewInt(0),
+		BerlinBlock:                   big.NewInt(0),
+		LondonBlock:                   big.NewInt(0),
+		ArrowGlacierBlock:             big.NewInt(0),
+		GrayGlacierBlock:              big.NewInt(0),
+		// TerminalTotalDifficulty:       MainnetTerminalTotalDifficulty, // 58_750_000_000_000_000_000_000
+		TerminalTotalDifficulty:       nil, // 58_750_000_000_000_000_000_000
+		TerminalTotalDifficultyPassed: false,
+		// ShanghaiTime:                  newUint64(1681338455),
+		// Ethash:                        new(EthashConfig),
+		ShanghaiTime:                  nil,
+		Ethash:                        nil,
+		Ichain:                        new(IchainConfig),
 	}
+
+	// // MainnetChainConfig is the chain parameters to run a node on the main network.
+	// MainnetChainConfig = &ChainConfig{
+	// 	ChainID:                       big.NewInt(1),
+	// 	HomesteadBlock:                big.NewInt(1_150_000),
+	// 	DAOForkBlock:                  big.NewInt(1_920_000),
+	// 	DAOForkSupport:                true,
+	// 	EIP150Block:                   big.NewInt(2_463_000),
+	// 	EIP155Block:                   big.NewInt(2_675_000),
+	// 	EIP158Block:                   big.NewInt(2_675_000),
+	// 	ByzantiumBlock:                big.NewInt(4_370_000),
+	// 	ConstantinopleBlock:           big.NewInt(7_280_000),
+	// 	PetersburgBlock:               big.NewInt(7_280_000),
+	// 	IstanbulBlock:                 big.NewInt(9_069_000),
+	// 	MuirGlacierBlock:              big.NewInt(9_200_000),
+	// 	BerlinBlock:                   big.NewInt(12_244_000),
+	// 	LondonBlock:                   big.NewInt(12_965_000),
+	// 	ArrowGlacierBlock:             big.NewInt(13_773_000),
+	// 	GrayGlacierBlock:              big.NewInt(15_050_000),
+	// 	TerminalTotalDifficulty:       MainnetTerminalTotalDifficulty, // 58_750_000_000_000_000_000_000
+	// 	TerminalTotalDifficultyPassed: true,
+	// 	ShanghaiTime:                  newUint64(1681338455),
+	// 	Ethash:                        new(EthashConfig),
+	// }
+
 	// HoleskyChainConfig contains the chain parameters to run a node on the Holesky test network.
 	HoleskyChainConfig = &ChainConfig{
 		ChainID:                       big.NewInt(17000),
@@ -362,9 +394,14 @@ type ChainConfig struct {
 	TerminalTotalDifficultyPassed bool `json:"terminalTotalDifficultyPassed,omitempty"`
 
 	// Various consensus engines
+	Ichain *IchainConfig `json:"ichain,omitempty"`
+
 	Ethash *EthashConfig `json:"ethash,omitempty"`
 	Clique *CliqueConfig `json:"clique,omitempty"`
 }
+
+// IchainConfig is the consensus engine configs for proof-of-inter-chain based sealing.
+type IchainConfig struct{}
 
 // EthashConfig is the consensus engine configs for proof-of-work based sealing.
 type EthashConfig struct{}
@@ -412,6 +449,8 @@ func (c *ChainConfig) Description() string {
 		} else {
 			banner += "Consensus: Beacon (proof-of-stake), merged from Clique (proof-of-authority)\n"
 		}
+	case c.Ichain != nil:
+		banner += "Consensus: IChain (proof-of-inter-chain)\n"
 	default:
 		banner += "Consensus: unknown\n"
 	}
